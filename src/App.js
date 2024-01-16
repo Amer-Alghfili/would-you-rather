fe25e2979e064b9641a407ea519192d7a3a64f86
Amer-Alghfili <ameralghfili@gmail.com>
2024-01-16 06:26:57 +0300

    will unmount

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
+import CreateQuestion from "./CreateQuestion";
+import Header from "./Header";
+import Leaderboard from "./Leaderboard";
+import QuestionInfo from "./QuestionInfo";
+import QuestionList from "./QuestionsList";
+import SignIn from "./SignIn";
 import { removeAuthedUser } from "./actions/authed";
 import getInitialData from "./actions/shared";
-import Header from "./Header";
 import protect from "./hoc/protect";
-import SignIn from "./SignIn";
-import CreateQuestion from "./CreateQuestion";
-import Leaderboard from "./Leaderboard";
-import QuestionList from "./QuestionsList";
-import QuestionInfo from "./QuestionInfo";
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(getInitialData());
   }
 
+  componentWillUnmount() {}
+
   removeAuthedUser = () => {
     this.props.dispatch(removeAuthedUser());
   };
   render() {
-    const {authed, user} = this.props;
+    const { authed, user } = this.props;
     return (
       <div>
         <Header user={user} logOut={this.removeAuthedUser} />
         <Switch>
           <Route path="/" exact component={SignIn} />
-          <Route path="/add" exact component={protect(CreateQuestion, authed)} />
-          <Route path="/leaderboard" exact component={protect(Leaderboard, authed)} />
+          <Route
+            path="/add"
+            exact
+            component={protect(CreateQuestion, authed)}
+          />
+          <Route
+            path="/leaderboard"
+            exact
+            component={protect(Leaderboard, authed)}
+          />
           <Route path="/home" exact component={protect(QuestionList, authed)} />
           <Route
             path="/questions/:id"
@@ -42,8 +52,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   user: state.users[state.authed],
-  authed: state.authed
+  authed: state.authed,
 });
 export default connect(mapStateToProps)(App);
